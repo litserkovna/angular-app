ff7885f95b3efd17dd3ddc8b6a2bef476318c7dc
litserkovna <39171450+litserkovna@users.noreply.github.com>
2024-03-14 12:01:02 +0100

    Updated task 2

--- cypress/e2e/homework14/task2.spec.js
+++ cypress/e2e/homework14/task2.spec.js
@@ -6,8 +6,8 @@ describe('Dialog Test', () => {
     it('Should click on the button', () => {
         cy.get('nb-card-body.result-from-dialog button').contains("Enter Name").click();
         cy.get('ngx-dialog-name-prompt.ng-star-inserted').should('exist');cy.get('ngx-dialog-name-prompt nb-card-header').should('contain.text', 'Enter your name');
-        cy.get('ngx-dialog-name-prompt nb-card-body input').should('exist');
-        cy.get('ngx-dialog-name-prompt nb-card-footer button').contains("Submit").should('exist');
-        cy.get('ngx-dialog-name-prompt nb-card-footer button').contains("Cancel").should('exist');
+        cy.get('ngx-dialog-name-prompt nb-card-body input').should('be.visible').and('be.enabled');
+        cy.get('ngx-dialog-name-prompt nb-card-footer button').contains("Submit").should('be.visible').and('be.enabled');
+        cy.get('ngx-dialog-name-prompt nb-card-footer button').contains("Cancel").should('be.visible').and('be.enabled');
     });
     });
